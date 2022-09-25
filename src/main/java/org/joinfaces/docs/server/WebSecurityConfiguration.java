f135080820605f59df4947e4c95c363c4bc36792
Lars Grefer <github@larsgrefer.de>
2022-09-26 01:23:18 +0200

    cleanup

--- src/main/java/org/joinfaces/docs/server/WebSecurityConfiguration.java
+++ src/main/java/org/joinfaces/docs/server/WebSecurityConfiguration.java
@@ -16,20 +16,23 @@
 
 package org.joinfaces.docs.server;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfiguration {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
                 .anyRequest().authenticated()
                 .and()
                 .csrf().disable()
                 .httpBasic();
+
+        return http.build();
     }
 }

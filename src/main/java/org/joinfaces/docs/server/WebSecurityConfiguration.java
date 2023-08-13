82f564babb13b93fca4f3066d8a070cf8ae3711f
Lars Grefer <github@larsgrefer.de>
2023-08-13 02:36:58 +0200

    Update Spring Security Deprecations

--- src/main/java/org/joinfaces/docs/server/WebSecurityConfiguration.java
+++ src/main/java/org/joinfaces/docs/server/WebSecurityConfiguration.java
@@ -18,7 +18,9 @@ package org.joinfaces.docs.server;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -27,11 +29,9 @@ public class WebSecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-                .authorizeRequests()
-                .anyRequest().authenticated()
-                .and()
-                .csrf().disable()
-                .httpBasic();
+                .authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests.anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
     }
